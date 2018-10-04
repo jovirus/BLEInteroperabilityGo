dff12a5fe6a06f857cf1d04a12202f28ca24976e
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-04 17:13:57 +0800

    update db connection info

--- dbConnnector.js
+++ dbConnnector.js
@@ -1,11 +1,13 @@
-const http = require('http');
-const hostname = '0.0.0.0';
-const port = 3000;
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello from the Jiajun\n');
-});
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
-});
+const MongoClient = require('mongodb').MongoClient
+
+// Note: A production application should not expose database credentials in plain text.
+// For strategies on handling credentials, visit 12factor: https://12factor.net/config.
+const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/admin"
+const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
+
+
+MongoClient.connect(PROD_URI, {useNewUrlParser: true}).then(() => {
+    if(!err) {
+        console.log("We are connected");
+      }
+})
