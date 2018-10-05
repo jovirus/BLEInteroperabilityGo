3a65c1290593fe3e81b42e6295f45ae5cff15e30
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-05 16:25:32 +0800

    update db connection info

---
+++ Database/dbConnnector.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient
+
+// Note: A production application should not expose database credentials in plain text.
+// For strategies on handling credentials, visit 12factor: https://12factor.net/config.
+const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/admin"
+
+
+function connect(url) {
+    MongoClient.connect(PROD_URI, {useNewUrlParser: true}).then(() => {
+        console.log("Database instance is conntected.");
+    }).catch((err) => {
+        console.log(`Database connection faild.${err.log}`)
+    })
+
+}
+
+module.exports = async function() {
+    let databases = await Promise.all([connect(PROD_URI)])
+   
+    return {
+      production: databases[0]
+    }
+}
