124957c5b6ac615a64d194afa2f39207996d104c
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-09 14:51:10 +0800

    correction grammar

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -1,7 +1,7 @@
 module.exports = function(app, dbs) {
 
     app.get('/testcase', (req, res) => {
-        dbo.tesNrf.collection('testcase').find({}).toArray((err, docs) => {
+        dbs.tesNrf.collection('testcase').find({}).toArray((err, docs) => {
           if (err) {
             console.log(err)
             res.error(err)
@@ -13,7 +13,7 @@ module.exports = function(app, dbs) {
       });
 
       app.get('/createDB', (req, res) => {
-      dbo.createCollection("testNrf002", function(err, res) { 
+      dbs.createCollection("testNrf002", function(err, res) { 
         if (err) throw err;
         console.log("Collection created!");
       })
