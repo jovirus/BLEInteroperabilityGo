0ed721d2423d0f7f60994f1780e191c2b704cc11
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-09 13:53:15 +0800

    grammar correction

--- Database/dbConnnector.js
+++ Database/dbConnnector.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/tesNrf"
+const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/admin"
 
 
 function connect(url) {
