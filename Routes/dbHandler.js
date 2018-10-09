2d25927cbaadfac00e2904db4c208b0f1a3fe875
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-09 17:16:31 +0800

    correction

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -6,15 +6,15 @@ module.exports = function(app, dbs) {
         dbs.collection('testcase').find().toArray((err, docs) => {
             if (err) throw err;
             console.log('Result found. See detail>')
-            // res.json(docs)
+            res.json(docs)
         })
       });
 
       app.get('/createDB', (req, res) => {
-      db.createCollection("testNrf002", function(err, res) { 
-        if (err) throw err;
-        console.log("Collection created!");
-      })
+        dbs.createCollection("testNrf002", function(err, res) { 
+            if (err) throw err;
+            console.log("Collection created!");
+        })
     })
     return app;
 }
