6f03a1a590b2ff5f2aa3ef22b6a29e177ee7f30f
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-09 12:19:14 +0800

    grammar correction

--- Database/dbConnnector.js
+++ Database/dbConnnector.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/admin"
+const PROD_URI = "mongodb://root:Nordic@dds-n9eafa9e8c2661841.mongodb.rds.aliyuncs.com:3717/testNrf"
 
 
 function connect(url) {
@@ -11,12 +11,17 @@ function connect(url) {
     }).catch((err) => {
         console.log(`Database connection faild.${err.log}`)
     })
+    // return MongoClient.connect(url).then(client => {
+    //     client.db()
+    //     console.log("Database instance is conntected.")
+    // }).catch((err) => {
+    //         console.log(`Database connection faild.${err.log}`)
+    //     })
 }
 
 module.exports = async function() {
     let databases = await Promise.all([connect(PROD_URI)]).catch(() => {})
     // let databases = new Promise(connect(PROD_URI)).catch(() => {})
-   
     return {
       production: databases[0]
     }
