ba3ab212c0dd27a5acb7f545d95ef7adc3604b24
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-10 16:26:10 +0800

    dev-routes

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -1,8 +1,9 @@
 const DATABASE_NAME = "tesNrf"
+const Joi = require("joi")
 
 module.exports = function(app, dbs) {
 
-    app.get('/testcase', (req, res) => {
+    app.get('/api/testcase', (req, res) => {
         console.log(`dbs contents ${dbs}`)
         let db = dbs.db(DATABASE_NAME);
         db.collection('testcase').find().toArray((err, docs) => {
@@ -11,6 +12,16 @@ module.exports = function(app, dbs) {
             res.json(docs)
             // dbs.close()
         })
+      });
+
+      app.post('/api/testcase', (req, res) => {
+        const schema = { 
+            brand: Joi.string().min(2).required()
+        }
+        const { error } = Joi.validate(req.body, schema)
+        if (!error) return status(400).send(error.detail[0].message)
+
+         res.send(req.body) 
       });
 
       app.get('/createDB', (req, res) => {
