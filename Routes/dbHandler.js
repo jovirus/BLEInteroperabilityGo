e64383aae210b3378a9c997e709324f179f5c91f
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-11 15:10:55 +0800

    dev-routes

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -7,28 +7,37 @@ module.exports = function(app, dbs) {
     app.use(express.json());
 
     app.get('/api/testcase', (req, res) => {
-        console.log(`dbs contents ${dbs}`)
         let db = dbs.db(DATABASE_NAME);
         db.collection('testcase').find().toArray((err, docs) => {
-            if (err) throw err;
-            console.log('Result found. See detail>')
+            if (err) throw err
             res.json(docs)
             // dbs.close()
         })
       });
 
       app.post('/api/testcase', (req, res) => {
-        const schema = { 
-            brand: Joi.string().min(2).required()
-        }
-        const { error } = Joi.validate(req.body, schema)
+        const { error } = validateBrandName(req.body)
         if (error) return res.status(400).send(error.details[0].message)
-
         const testcase = {
             brand: req.body.brand
         }
 
          res.send(testcase) 
+      });
+
+      app.put('/api/testcase/:brand', (req, res) => {
+        let vari = req.params.brand
+        const { error } = validateBrandName(vari)
+        if (error) return res.status(400).send(error.details[0].message)
+
+        let db = dbs.db(DATABASE_NAME);
+        const query = {
+            brand: vari
+        }
+        const testcase = db.collection('testcase').find({query}).toArray((err, docs) => {
+            if (err) throw err
+            res.json(docs)
+        })
       });
 
       app.get('/createDB', (req, res) => {
@@ -38,5 +47,12 @@ module.exports = function(app, dbs) {
             console.log("Collection created!");
         })
     })
+
+    function validateBrandName(testcase){
+        const schema = { 
+            brand: Joi.string().min(2).required()
+        }
+        return Joi.validate(testcase, schema)
+    }
     return app;
 }
