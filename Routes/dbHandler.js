35b0e4378c77a6d3dce77de1e85acb87e710dbaf
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-15 13:23:40 +0800

    update routes to handel request

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -11,53 +11,87 @@ module.exports = function(app, dbs) {
         db.collection('testcase').find().toArray((err, docs) => {
             if (err) throw err
             res.json(docs)
-            // dbs.close()
         })
       });
 
-      app.post('/api/testcase', (req, res) => {
-        const { error } = validateBrandName(req.body)
+      app.post('/api/insert/testreport', (req, res) => {
+        let report = req.body
+        const { error } = validateTestReport(report)
         if (error) return res.status(400).send(error.details[0].message)
-        const testcase = {
-            brand: req.body.brand
-        }
-
-         res.send(testcase) 
+        let db = dbs.db(DATABASE_NAME)
+        let result = db.collection("testReport").insertOne(report, function(err, res){
+            if (err) return res.send(err)
+        }) 
+         res.send(result) 
       });
 
-      app.put('/api/testcase/:brand', (req, res) => {
-        let vari = req.params.brand
-        // let body = req.body
-        // const { error } = validateBrandName(body)
-        console.log(vari)
-        // console.log(body)
-        // if (error) return res.status(400).send(error.details[0].message)
-
-        let db = dbs.db(DATABASE_NAME);
-        var query = {
-            brand: vari 
-        };
-        // res.send(query)
-        db.collection('testcase').find(query).toArray((err, docs) => {
-            if (err) throw err
-            console.log(docs)
-            res.send(docs)
-        })
+      app.post('/api/insert/mobileinfo', (req, res) => {
+        let mobileinfo = req.body
+        const { error } = validateMobileInfo(req.body)
+        if (error) return res.status(400).send(error.details[0].message)
+        let db = dbs.db(DATABASE_NAME)
+        let result = db.collection("mobileInfo").insertOne(report, function(err, res){
+            if (err) return res.send(err)
+        }) 
+         res.send(result)
       });
 
-      app.get('/createDB', (req, res) => {
-        let db = dbs.db(DATABASE_NAME);
-        db.createCollection("testNrf002", function(err, res) { 
-            if (err) throw err;
-            console.log("Collection created!");
-        })
-    })
+    //   app.put('/api/find/testreport/:brand', (req, res) => {
+    //     let vari = req.params.brand
+    //     let db = dbs.db(DATABASE_NAME);
+    //     var query = {
+    //         brand: vari 
+    //     };
+    //     db.collection('testcase').find(query).toArray((err, docs) => {
+    //         if (err) throw err
+    //         res.send(docs)
+    //     })
+    //   });
 
-    function validateBrandName(testcase){
+      app.get('/api/initialize', (req, res) => {
+        let db = dbs.db(DATABASE_NAME);
+        db.createCollection("testReport", {capped: true, autoIndexId:true})
+        db.createCollection("mobileInfo", {capped: true, autoIndexId:true})
+      })
+
+    function validateBrandName(value){
         const schema = { 
             brand: Joi.string().min(2).required()
         }
-        return Joi.validate(testcase, schema)
+        return Joi.validate(value, schema)
     }
+
+    function validateTestReport(value) {
+        const schema = {
+            sessionID: Joi.string().required(),
+            weChatVersion: Joi.string().required(),
+            sdkVersion: Joi.string().required(),
+            timeStamp: Joi.date().required(),
+            testType: Joi.string().required(),
+            mobileInfoID: Joi.string().required(),
+            peripheralInfoID: Joi.string(),
+            isPassed: Joi.boolean().required()
+        }
+        return Joi.validate(value, schema)
+    }
+
+    function validateMobileInfo(value) {
+        const schema = {
+            brand: Joi.string().required(),
+            model: Joi.string().required(),
+            platform: Joi.string().required()
+        }
+        return Joi.validate(value, schema)
+    }
+
+    function validatePeripheralInfo(value) {
+        const schema = {
+            chipset: Joi.string().required(),
+            softdevice: Joi.string().required(),
+            application: Joi.string().required(),
+            bootloader: Joi.string().required()
+        }
+        return Joi.validate(value, schema)
+    } 
     return app;
 }
