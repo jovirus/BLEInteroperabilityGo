504e77c4f93db196bde7197cd77d285012da8f84
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-10-16 17:42:57 +0800

    update routes

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -46,7 +46,7 @@ module.exports = function(app, dbs) {
         var query = {
             mobileInfoID: id 
         };
-        db.collection('TestReport').findOne(query, (err, docs) => {
+        db.collection('TestReport').find(query, (err, docs) => {
             if (err) throw err
             res.send(docs)
         })
