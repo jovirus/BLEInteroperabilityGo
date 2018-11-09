b93b1cfa46f2b0e684f6d8c112e0362da724e4e6
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-11-09 15:56:40 +0800

    routes

--- Database/dbConnnector.js
+++ Database/dbConnnector.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
-const PROD_URI = "mongodb://root:Nordic119@dds-n9ed9c485fb5de941.mongodb.rds.aliyuncs.com:3717"
+const PROD_URI = "mongodb://root:Nordic119@dds-n9ed9c485fb5de941.mongodb.rds.aliyuncs.com:3717,dds-n9ed9c485fb5de942.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-10430867"
 
 
 function open() {
