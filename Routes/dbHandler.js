3cc589638cc22cf2d6362f2b086ba7b52a6c541c
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-11-13 14:05:32 +0800

    routes

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -173,12 +173,12 @@ module.exports = function(app, dbs) {
     function validateTesterInfo(value) {
         const schema = {
             nickName: Joi.string().allow(''),
-            gender: Joi.string().allow(''),
+            gender: Joi.number().optional(),
             language: Joi.string().allow(''),
             city: Joi.string().allow(''),
             province: Joi.string().allow(''),
             contry: Joi.string().allow(''),
-            avatarUrl: Joi.string().allow('')
+            avatarUrl: Joi.string().allow('').optional()
         }
         return Joi.validate(value, schema)
     }
