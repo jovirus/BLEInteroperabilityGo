ab07a7b60f6890131cec4cce7c966994aa2e9c02
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-11-26 14:48:44 +0800

    route

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const port = process.env.PORT || 80
 
 initializeDatabases.open().then(dbs => { 
     routes(app, dbs).listen(port, () => {
-        const httpServer = http.createServer(this).apply(httpServer)
-        // return httpServer.listen(port).apply(httpServer)
+        const httpServer = http.createServer(this)
+        return httpServer.listen.apply(httpServer)
         console.log(`listening on port ${port}`)
     })
 })
