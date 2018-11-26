bfe1d44a9d4172f738cd8bbad6857cce5f48d51d
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-11-26 16:45:43 +0800

    route

--- Database/dbConnnector.js
+++ Database/dbConnnector.js
@@ -1,10 +1,17 @@
 
+/** This is file is used to handel web requests from clients and store in DB.
+ *  Usage: nrf devices interoperability test. No other purpose allowed.
+ *  created by Jiajun Qiu @Nordic Semiconductor ASA 2018
+ *  Email: jiajun.qiu@nordicsemi.no
+ *  Lisence: GPL3.0
+ *  See more detail on https://github.com/jovirus/ble-interoperabilityTest
+ */
+
 const MongoClient = require('mongodb').MongoClient
 
 // Note: A production application should not expose database credentials in plain text.
 // For strategies on handling credentials, visit 12factor: https://12factor.net/config.
 const PROD_URI = "mongodb://root:Nordic119@dds-n9ed9c485fb5de941.mongodb.rds.aliyuncs.com:3717,dds-n9ed9c485fb5de942.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-10430867"
-
 
 function open() {
     return new Promise((resolve, reject) => {
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ble-interoperabilityTest
-This is repo is created for BLE interoperability Testing.
+This project is on target to support nrf device interoperability test. It wires other services hosted by NordicSemi APAC.
+
+*NordicSemi retain full rights to make changes or limit the api access at anytime.
+*NordicSemi might collect test results for furthur analyse.
+*NordicSemi will not furtur transfer any user information to third party.
+
+By using the services, user shall accept this terms and conditions.
+
+Nordic Semiconductor ASA 2018
