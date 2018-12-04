0c351027cf79a54cf13438c159309ccb905c8689
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-12-04 17:22:14 +0800

    update https

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var credentials = {key: privateKey, cert: certificate};
 initializeDatabases.open().then(dbs => { 
     routes(app, dbs).listen(() => {
         const httpServer = http.createServer(app).listen(80)
-        // const httpsServer = https.createServer(credentials, app)
+        const httpsServer = https.createServer(credentials, app).listen(443)
         // console.log(`listening on port ${port}`)
         // httpsServer.listen.apply(httpsServer)
     })
