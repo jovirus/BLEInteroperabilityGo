6c24b373bc63d5b1f29eaeb61ae02d615bbbb201
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2018-12-13 15:28:47 +0800

    add nrf91 test

--- Routes/dbHandler.js
+++ Routes/dbHandler.js
@@ -166,7 +166,9 @@ module.exports = function(app, dbs) {
     }
     db.collection(process.env.DB_TEST_COLLECTION_NRF91).findOneAndUpdate(query, {$set: newValue}, {upsert: true}, (err, docs) => {
         if (err) return res.status(400).send(err)
-        res.status(200).send(docs)
+        let result = JSON.parse(docs).lastErrorObject.updatedExisting
+        console.log(`Updating existing ${result}`)
+        res.status(200).send(result)
         })
     })
     // let result = db.collection(process.env.DB_TEST_COLLECTION_NRF91).insertOne(report, function(err, object){
