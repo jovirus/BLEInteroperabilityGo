286305daf43b0dcf618e11aedfe10db0b5440437
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2019-02-22 12:35:42 +0800

    wechat login

--- Routes/loginService.js
+++ Routes/loginService.js
@@ -52,7 +52,7 @@ function readCookie(name) {
 
 function getWxLoginQRCode() {
     return new Promise((resolve, reject) => {
-        const options = new URL(`https://open.weixin.qq.com/connect/qrconnect?appid=${process.env.LOGIN_WX_APP_ID}&redirect_uri=${process.env.LOGIN_WX_REDIRECT_URL}&response_type=code&scope=snsapi_login&state=STATE`)
+        const options = new URL(`https://open.weixin.qq.com/connect/qrconnect?appid=${process.env.LOGIN_WX_APP_ID}&redirect_uri=${process.env.LOGIN_WX_REDIRECT_URL}&response_type=code&scope=snsapi_userinfo&state=STATE`)
         networkHandler.httpsRequest(options).then((rawHTML) => {
             var modifiedResult = rawHTML.replace("/connect/qrcode/", "https://open.weixin.qq.com/connect/qrcode/")
             resolve(modifiedResult)
