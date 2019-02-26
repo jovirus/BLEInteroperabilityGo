484efe1620398d60d7273769a6a1af5cadf94859
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2019-02-26 17:06:55 +0800

    wechat login

--- Routes/loginService.js
+++ Routes/loginService.js
@@ -8,6 +8,7 @@
 var seedrandom = require('seedrandom')
 const networkHandler = require('./networkHandler')
 var cookie = require('cookie')
+var url = require('url')
 
 
 function random() {
@@ -21,19 +22,42 @@ function randomWithSeed(seed) {
 }
 
 function setCookie(req, res) {
-// Parse the query string
-
-    // Set a new cookie with the name
-    res.setHeader('Set-Cookie', cookie.serialize('nrf', String("token"), {
-    httpOnly: true,
-    maxAge: 60 * 60 * 24 * 7 // 1 week
-    }));
-
-    // Redirect back after setting cookie
-    res.statusCode = 302;
-    res.setHeader('Location', req.headers.referer || '/index.html');
-    res.send()
-    return res
+    function onRequest(req, res) {
+        // Parse the query string
+        var query = url.parse(req.url, true, true).query;
+       
+        if (query && query.name) {
+          // Set a new cookie with the name
+          res.setHeader('Set-Cookie', cookie.serialize('name', String(query.name), {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7 // 1 week
+          }));
+       
+          // Redirect back after setting cookie
+          res.statusCode = 302;
+          res.setHeader('Location', req.headers.referer || '/');
+          res.end();
+          return;
+        }
+       
+        // Parse the cookies on the request
+        var cookies = cookie.parse(req.headers.cookie || '');
+       
+        // Get the visitor name set in the cookie
+        var name = cookies.name;
+       
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+       
+        if (name) {
+          res.write('<p>Welcome back, <b>' + escapeHtml(name) + '</b>!</p>');
+        } else {
+          res.write('<p>Hello, new visitor!</p>');
+        }
+       
+        res.write('<form method="GET">');
+        res.write('<input placeholder="enter your name" name="name"> <input type="submit" value="Set Name">');
+        res.end('</form');
+      }
 }
 
 function writeCookie(name,value,days) {
