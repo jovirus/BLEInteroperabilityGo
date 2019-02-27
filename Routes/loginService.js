a922eddb477705a0f09677d0ba88749bf17ae898
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2019-02-27 17:21:43 +0800

    wechat login

--- Routes/loginService.js
+++ Routes/loginService.js
@@ -43,18 +43,18 @@ function writeCookie(name,value,days) {
  */
 function readCookie(cname) {
     var name = cname + "="; //Create the cookie name variable with cookie name concatenate with = sign
-    var cArr = document.cookie.split(';'); //Create cookie array by split the cookie by ';'
     if (typeof document !== 'undefined') {
-    //Loop through the cookies and return the cooki value if it find the cookie name
-    for(var i=0; i<cArr.length; i++) {
-        var c = cArr[i].trim();
-        //If the name is the cookie string at position 0, we found the cookie and return the cookie value
-        if (c.indexOf(name) == 0) 
-            return c.substring(name.length, c.length);
-    }
-    //If we get to this point, that means the cookie wasn't find in the look, we return an empty string.
-    return "";
-    }
+        var cArr = document.cookie.split(';'); //Create cookie array by split the cookie by ';'
+        //Loop through the cookies and return the cooki value if it find the cookie name
+        for(var i=0; i<cArr.length; i++) {
+            var c = cArr[i].trim();
+            //If the name is the cookie string at position 0, we found the cookie and return the cookie value
+            if (c.indexOf(name) == 0) 
+                return c.substring(name.length, c.length);
+        }
+        //If we get to this point, that means the cookie wasn't find in the look, we return an empty string.
+        return "";
+        }
 }
 
 /**
