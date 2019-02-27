e1816016383a4665dff1581a5010284a31881999
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2019-02-27 11:15:07 +0800

    wechat login

--- Routes/routerHandler.js
+++ Routes/routerHandler.js
@@ -65,7 +65,7 @@ module.exports = function(app, dbs) {
                     } else {
                         // send cookies
                         // var res1 = loginService.setCookie(req, res)
-                        res.cookie("nrfa", 'cookie_value1', { expires: new Date(Date.now() + 60), httpOnly: true, secure: true, signed: true });
+                        res.cookie("nrfa1", 'cookie_value1', { expires: new Date(Date.now() + 60), httpOnly: true, secure: true, signed: true });
                         res.send("all cookie is set.")
                     }
                 })
