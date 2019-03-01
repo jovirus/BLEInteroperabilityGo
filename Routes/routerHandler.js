32099515e6d2da6ef9e2588fe57eeaabd96778cf
Jiajun Qiu <jiajun.qiu@nordicsemi.no>
2019-03-01 14:51:12 +0800

    wechat login

--- Routes/routerHandler.js
+++ Routes/routerHandler.js
@@ -53,13 +53,8 @@ module.exports = function(app, dbs) {
         let wxCode = req.query.code
         console.log('Redirect1 Cookies: ', req.cookies)
         console.log('Redirect1 Signed Cookies: ', req.signedCookies)
-        console.log("type of cookie",typeof(req.signedCookies))
-        if (req.signedCookies.t === undefined) {
+        if (req.signedCookies.t !== undefined) {
             console.log("signed cookie... undefined",req.signedCookies.t)
-        }
-        if (req.signedCookies.t !== {}) {
-            console.log("signed cookie... {}",req.signedCookies.t)
-
             dataStorageService.isExistCookie(dbs, signedCookie).then((isExist) => {
                 if (isExist) {
                     res.redirect(`https://nrfipa.com/api/index.html?user=XXXX`);
